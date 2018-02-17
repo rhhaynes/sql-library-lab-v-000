b5a447ec0c1293debedd07648c4e342a753f37c1
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 16:30:21 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,3 @@
-UPDATE characters SET species = "Martian" WHERE id = ( SELECT MAX(id) FROM characters )
+UPDATE characters
+SET    species = "Martian"
+WHERE  id = ( SELECT MAX(id) FROM characters )
