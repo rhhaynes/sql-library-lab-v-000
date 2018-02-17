b7c3ec3dbc5d5efe107f31dca1b6a5487587141e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 16:34:32 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  %q{SELECT   
-     
+  %q{SELECT   books.title, books.year
+     FROM     books
+     JOIN     series ON series.book
+     ORDER BY books.year
   }
 end
 
