5d36b66a3b81f502c3eead3282819952794d74fb
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 17:12:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,5 +38,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-
+  %q{SELECT characters.name, COUNT(books.name)
+     FROM   characters
+     JOIN   books ON 
+  }
 end
