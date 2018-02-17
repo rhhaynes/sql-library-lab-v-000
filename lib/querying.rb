fc6369678386b2ad7e23ef2523bad22970681403
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 17:10:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,8 @@ end
 def select_value_and_count_of_most_prolific_species
   %q{SELECT   characters.species, COUNT(*)
      FROM     characters
-     GROUP BY characters.species ORDER BY COUNT(*) DESC LIMIT 1
+     GROUP BY characters.species
+     ORDER BY COUNT(*) DESC LIMIT 1
   }
 end
 
