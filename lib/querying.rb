b55c9ba6fbcd7083fd72253f9dc1d1863eec551e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 16:59:02 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
   %q{SELECT   authors.name, subgenres.name
      FROM     authors
      JOIN     series    ON series.author_id = authors.id
-     JOIN     subgenres ON subgenres.id = series.subgenres_id
+     JOIN     subgenres ON subgenres.id = series.subgenre_id
   }
 end
 
