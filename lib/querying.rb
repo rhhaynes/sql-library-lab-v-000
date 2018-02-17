ba0d5477fdb198ebf1a77879d37f28b4095eaf6c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 17:05:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_series_title_with_most_human_characters
   %q{SELECT   series.title
      FROM     series
      JOIN     characters ON characters.series_id = series.id
-     WHERE 
+     ORDER BY COUNT(*) WHERE character.species = 'human'
   }
 end
 
