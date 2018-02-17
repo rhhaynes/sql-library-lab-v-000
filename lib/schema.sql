cbb30c93841f738116a29efcf62d41f7184e34dd
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 16:19:07 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -15,3 +15,25 @@ CREATE TABLE authors (
   name        TEXT
 );
 
+CREATE TABLE books (
+  id          INTEGER PRIMARY KEY,
+  title       TEXT,
+  year        TEXT,
+  series_id   INTEGER
+);
+
+CREATE TABLE characters (
+  id          INTEGER PRIMARY KEY,
+  name        TEXT,
+  species     TEXT,
+  motto       TEXT,
+  series_id   INTEGER,
+  author_id   INTEGER
+);
+
+CREATE TABLE series (
+  id          INTEGER PRIMARY KEY,
+  title       TEXT,
+  author_id   INTEGER,
+  subgenre_id INTEGER
+);
