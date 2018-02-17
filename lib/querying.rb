7eba8387d0c141e8461e57dc07504a3d6ed2f2b3
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 16:51:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,9 +12,11 @@ def select_name_and_motto_of_char_with_longest_motto
   }
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  %q{SELECT   species.name, COUNT(*)
+     FROM     species
+     GROUP BY species.name ORDER BY COUNT(*) DESC LIMIT 1
+  }
 end
 
 def select_name_and_series_subgenres_of_authors
